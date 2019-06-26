48954c24a68be582d2d12dccf8636ccb60f5af06
Tommy Scribble <tommy@scribbledesign.co.uk>
2019-06-26 07:02:34 +0100

    add new logo to library

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import Heading from '../objects/Heading';
 import SideBar from './SideBar';
 
 const Header = props => {
-  return (
-    <header>
-      <nav className="top-nav">
-        <Heading tagNumber={1} headingText={'This is a heading component'} />
-      </nav>
-      <SideBar />
-    </header>
-  );
+	return (
+		<header>
+			<nav className="top-nav">
+				<Heading tagNumber={1} headingText={'React UI component library'} />
+			</nav>
+			<SideBar />
+		</header>
+	);
 };
 
 export default Header;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import myLogo from '../assets/scribble_logo.png';
+import myLogo from '../assets/scribble_logo--white.png';
 import Logo from './../objects/Logo';
 import Navigation from './Navigation';
 
 const SideBar = props => {
-  const versionNo = 'Version 1.0.0';
-  return (
-    <ul className="side-nav fixed">
-      <li className="logo">
-        <Logo anchor={'./'} logo={myLogo} altText={'Pattern Library 2019'} />
-      </li>
-      <li className="version">{versionNo}</li>
-      <Navigation />
-    </ul>
-  );
+	const versionNo = 'Version 1.0.0';
+	return (
+		<ul className="side-nav fixed">
+			<li className="logo">
+				<Logo anchor={'./'} logo={myLogo} altText={'Pattern Library 2019'} />
+			</li>
+			<li className="version">{versionNo}</li>
+			<Navigation />
+		</ul>
+	);
 };
 
 export default SideBar;
