4c6958db66b837781d2bfa4b0f6db6683bb35d5e
Tommy Scribble <tommy@scribbledesign.co.uk>
2019-06-26 07:26:13 +0100

    add basic content and some styles to the homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
+import Heading from '../objects/Heading';
 import Text from '../objects/Text';
+import ButtonAnchor from '../objects/ButtonAnchor';
 
 const homepage = props => {
 	return (
 		<PageLayout>
-			<Text>This is the homepage</Text>
+			<Heading
+				tagNumber={3}
+				headingText={
+					'This pattern library is currently still under construction'
+				}
+			/>
+			<Text>
+				Navigate using the sidebar to the element you would like to use to find
+				out more
+			</Text>
+			<Text>To get involved use one of the links below</Text>
+			<ButtonAnchor path={'/buttons'} anchorClass={'button'} internal={true}>
+				Get started
+			</ButtonAnchor>
+			<ButtonAnchor
+				path={'https://github.com/TommyScribble/pattern-library'}
+				anchorClass={'button'}>
+				View Git
+			</ButtonAnchor>
 		</PageLayout>
 	);
 };
