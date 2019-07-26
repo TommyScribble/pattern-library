b1c4180b106a5973f27c86a963007b2beb40e655
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2019-07-26 15:09:19 +0100

    add bulma classes to accordion page markup

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -3,13 +3,12 @@ import Header from './Header';
 // import Footer from './Footer';
 
 const PageLayout = props => {
-  return (
-    <React.Fragment>
-      <Header />
-      <main className="main-content">{props.children}</main>
-      {/* <Footer /> */}
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Header />
+			<main className="main-content">{props.children}</main>
+		</React.Fragment>
+	);
 };
 
 export default PageLayout;
--- src/pages/accordion.js
+++ src/pages/accordion.js
@@ -7,59 +7,63 @@ import Heading from '../objects/Heading';
 const accordion = props => {
 	return (
 		<PageLayout>
-			<Text>This is the accordion page</Text>
-			<Heading tagNumber={4} headingText={'Single Open'} />
-			<Accordion>
-				<div title={'Alligator Mississippiensis'} isOpen={false}>
-					<div>
-						<strong>Common Name:</strong> American Alligator
-					</div>
-					<div>
-						<strong>Distribution:</strong> Texas to North Carolina, United
-						States
-					</div>
-					<div>
-						<strong>Endangered Status:</strong> Currently Not Endangered
-					</div>
+			<section className="section">
+				<div className="container">
+					<Text>This is the accordion page</Text>
+					<Heading tagNumber={4} headingText={'Single Open'} />
+					<Accordion>
+						<div title={'Alligator Mississippiensis'} isOpen={false}>
+							<div>
+								<strong>Common Name:</strong> American Alligator
+							</div>
+							<div>
+								<strong>Distribution:</strong> Texas to North Carolina, United
+								States
+							</div>
+							<div>
+								<strong>Endangered Status:</strong> Currently Not Endangered
+							</div>
+						</div>
+						<div title={'Alligator Sinensis'} isOpen={false}>
+							<div>
+								<strong>Common Name:</strong> Chinese Alligator
+							</div>
+							<div>
+								<strong>Distribution:</strong> Eastern China
+							</div>
+							<div>
+								<strong>Endangered Status:</strong> Critically Endangered
+							</div>
+						</div>
+					</Accordion>
+					<Heading tagNumber={4} headingText={'Multiple Open'} />
+					<Accordion allowMultipleOpen>
+						<div title={'Alligator Mississippiensis'} isOpen={false}>
+							<div>
+								<strong>Common Name:</strong> American Alligator
+							</div>
+							<div>
+								<strong>Distribution:</strong> Texas to North Carolina, United
+								States
+							</div>
+							<div>
+								<strong>Endangered Status:</strong> Currently Not Endangered
+							</div>
+						</div>
+						<div title={'Alligator Sinensis'} isOpen={false}>
+							<div>
+								<strong>Common Name:</strong> Chinese Alligator
+							</div>
+							<div>
+								<strong>Distribution:</strong> Eastern China
+							</div>
+							<div>
+								<strong>Endangered Status:</strong> Critically Endangered
+							</div>
+						</div>
+					</Accordion>
 				</div>
-				<div title={'Alligator Sinensis'} isOpen={false}>
-					<div>
-						<strong>Common Name:</strong> Chinese Alligator
-					</div>
-					<div>
-						<strong>Distribution:</strong> Eastern China
-					</div>
-					<div>
-						<strong>Endangered Status:</strong> Critically Endangered
-					</div>
-				</div>
-			</Accordion>
-			<Heading tagNumber={4} headingText={'Multiple Open'} />
-			<Accordion allowMultipleOpen>
-				<div title={'Alligator Mississippiensis'} isOpen={false}>
-					<div>
-						<strong>Common Name:</strong> American Alligator
-					</div>
-					<div>
-						<strong>Distribution:</strong> Texas to North Carolina, United
-						States
-					</div>
-					<div>
-						<strong>Endangered Status:</strong> Currently Not Endangered
-					</div>
-				</div>
-				<div title={'Alligator Sinensis'} isOpen={false}>
-					<div>
-						<strong>Common Name:</strong> Chinese Alligator
-					</div>
-					<div>
-						<strong>Distribution:</strong> Eastern China
-					</div>
-					<div>
-						<strong>Endangered Status:</strong> Critically Endangered
-					</div>
-				</div>
-			</Accordion>
+			</section>
 		</PageLayout>
 	);
 };
