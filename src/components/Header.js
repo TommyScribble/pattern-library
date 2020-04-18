ea7204bd73b5443f8c89d30f537feb86d08b35dc
Tommy Scribble <tommy@scribbledesign.co.uk>
2020-04-18 09:05:43 +0100

    refactor header and sidebar so logic resides in correct comoponents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,22 @@
 import React, { useState } from 'react';
 import Heading from '../objects/Heading';
 import SideBar from './SideBar';
-// import Icon from 'react-icon-library';
+import Icon from 'react-icon-library';
 
 const Header = () => {
-	let [currentIcon, updateIcon] = useState('Burger1');
-
-	const sideBar = document.getElementsByClassName('side-nav');
-
-	let burgerIcon = currentIcon;
-
-	const openCloseNav = () => {
-		// This needs to trigger a re-render of ther burgerIcon so needs to refacotr a;ll to use react hooks
-		if (sideBar[0].classList.contains('js-open')) {
-			sideBar[0].classList.remove('js-open');
-			sideBar[0].classList.add('js-close');
-		} else {
-			sideBar[0].classList.remove('js-close');
-			sideBar[0].classList.add('js-open');
-		}
-	};
+	const [icon, updateIcon] = useState('Burger1');
 
 	return (
 		<header>
 			<nav className="top-nav">
-				<Heading tagNumber={1} headingText={'React UI library'} />
+				<Heading tagNumber={1} headingText={'ReactJs UI library'} />
 				<button
 					className="burger-menu"
-					onClick={() => {
-						openCloseNav();
-						updateIcon(currentIcon === 'Cross1' ? 'Burger1' : 'Cross1');
-					}}>
-					{/* <Icon iconName={burgerIcon} /> */}
+					onClick={() => updateIcon(icon === 'Burger1' ? 'Cross1' : 'Burger1')}>
+					<Icon iconName={icon} />
 				</button>
 			</nav>
-			<SideBar />
+			<SideBar active={icon} />
 		</header>
 	);
 };
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import myLogo from '../assets/scribble_logo--white.png';
 import Logo from './../objects/Logo';
 import Navigation from './Navigation';
 
-const SideBar = props => {
+const SideBar = ({ active }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	useEffect(() => {
+		setIsOpen(active === 'Burger1' ? false : true);
+	}, [active]);
+
+	const openClass = isOpen === true ? 'js-open' : 'js-close';
+
 	const versionNo = 'Version 1.0.0';
 
 	return (
-		<ul className="side-nav fixed js-close">
+		<ul className={`side-nav fixed ${openClass}`}>
 			<li className="logo">
 				<Logo anchor={'./'} logo={myLogo} altText={'Pattern Library 2019'} />
 			</li>
